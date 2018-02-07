9f0154a789a3611161772b6e7b4ff77c368b1f97
ZSI2017 <13407121461@163.com>
2018-02-07 23:52:54 +0800

    orientation change

--- neolix/public/js/main.js
+++ neolix/public/js/main.js
@@ -1,44 +1,51 @@
 $(function () {
-	var clientHeight =  $(document).height();
-	var clientWidth =  $(document).width();
-	var windowWidth = $("body").width();
-	calHeight();
+	var scrollHeight =  $(document).height(),
+	    scrollWidth =  $(document).width(),
+			isMobile = /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent);
+	$(".device").css('width',scrollHeight * 0.24)  // 初始化滚动条的width
+
 	if ($(document).width() <= 414) {
 		$("#car").attr("src", "/public/images/mobile/car.png")
 		$(".marquee-move img").attr("src", "/public/images/mobile/text.png")
-	} else if(!isPc()){
+	} else if(orientation() === "landscape"){
 		$("#car").attr("src", "/public/images/mobile/orientation/car.png");
-	 var marqueeDevWidth = Math.min(clientWidth,$(document).height()) * 0.34
-		$(".device").css('width',marqueeDevWidth)
+		$(".device").css('width',Math.min(scrollWidth,scrollHeight) * 0.34)
 	}
 	$("img").each(function (index, item) {
 		$(this).css('visibility', 'visible')
 	})
 
-	function calHeight() {
-		var clientHeight = $(document).height();
-		var marqueeDevWidth = clientHeight * 0.24;
-    $(".device").css('width',marqueeDevWidth)
+ /**
+  * 判断 移动端 初始的 横屏（"landscape"）还是竖屏 （"portrait"）
+  */
+	function orientation() {
+		if(isMobile){
+			if (window.orientation === 180 || window.orientation === 0||($(window).height()>$(window).width())) {
+				 return "portrait"
+			}else if(window.orientation === 90 || window.orientation === -90||($(window).height()<$(window).width())) {
+				// 横向
+				return "landscape"
+			}else {
+				return false
+			}
+		}
+		return false;
 	}
 
-  function isPc() {
-		if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-			return false;
-		}else {
-			return true;
-		}
-   }
-
+ /**
+  * 监听横屏竖屏切换， 设置对应车的图片 和 滚动条的width;
+  * @return {[type]} [description]
+  */
 	window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
-				if (window.orientation === 180 || window.orientation === 0) {
-					 $("#car").attr("src", "/public/images/mobile/car.png");
-					 var marqueeDevWidth = Math.max(clientHeight,clientWidth) * 0.24;
-			     $(".device").css('width',marqueeDevWidth)
-				}
-				if (window.orientation === 90 || window.orientation === -90 ){
-					 $("#car").attr("src", "/public/images/mobile/orientation/car.png");
-			 		var marqueeDevWidth = Math.min(clientWidth,$(document).height()) * 0.34
-			     $(".device").css('width',marqueeDevWidth)
-				}
-		}, false);
+		if(isMobile){
+			if (window.orientation === 180 || window.orientation === 0||($(window).height()>$(window).width())) {
+				 $("#car").attr("src", "/public/images/mobile/car.png");
+				 $(".device").css('width',$(document).height()* 0.24)
+			}
+			if (window.orientation === 90 || window.orientation === -90||($(window).height()<$(window).width())){
+				 $("#car").attr("src", "/public/images/mobile/orientation/car.png");
+				 $(".device").css('width',Math.min(scrollWidth,$(document).height()) * 0.34)
+			}
+		}
+	}, false);
 })
